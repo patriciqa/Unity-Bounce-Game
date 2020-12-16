f182ddfe64d39d6e09fcc1c7711393b2440340a9
patriciaabderhalden <patty-ab@windowslive.com>
2020-12-16 17:21:31 +0100

    add sound

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -6,14 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float Speed;
+    public AudioSource tickSource;
 
+        private void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
 
     //Player cannot move when collided
-   public void Update()
+    public void Update()
     {
          Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
          transform.Translate(movementVector * Time.deltaTime * Speed);
     }
 
-   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            tickSource.Play();
+        }
+    }
+
+
 }
