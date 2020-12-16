f9a9a66a8612e27fb05e2435114ae53083ed0025
sprzrg <58468359+sprzrg@users.noreply.github.com>
2020-12-16 16:56:41 +0100

    level-build

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -6,14 +6,33 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float Speed;
+    private bool Move = true;
+    public float Delay = 1f;
 
 
     //Player cannot move when collided
-   public void Update()
+    public void Update()
     {
-         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
-         transform.Translate(movementVector * Time.deltaTime * Speed);
+        if(Move == true)
+        {
+            Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
+            transform.Translate(movementVector * Time.deltaTime * Speed);
+        }
+        
     }
 
-   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(CollisionRoutine());
+    }
+
+
+    IEnumerator CollisionRoutine()
+    {
+        Move = false;
+        yield return new WaitForSeconds(Delay);
+        Move = true;
+    }
+
 }
