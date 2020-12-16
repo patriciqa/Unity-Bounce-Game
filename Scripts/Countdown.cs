00bd6debf14e3898ff7342e5acde8f91022066df
patriciaabderhalden <patty-ab@windowslive.com>
2020-12-16 12:23:32 +0100

    counter starts when player moves

--- Scripts/Countdown.cs
+++ Scripts/Countdown.cs
@@ -9,29 +9,42 @@ using TMPro;
 public class Countdown : MonoBehaviour
 {
     float currentTime = 0f;
-    public float startingTime;       
+    public float startingTime;
     public TextMeshProUGUI text;
-
+    bool started = false;
 
     private void Start()
     {
         currentTime = startingTime;
+        started = false;
     }
+
+    private void delayedTime() {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            started = true;
+        }
+    }
+
+    
 
     void Update()
     {
-        currentTime -= 1 * Time.deltaTime;
-        text.text = currentTime.ToString("0");
-
-        if (currentTime <= 0)
+        delayedTime();
+        if (started == true)
         {
-            currentTime = 0;
-            FindObjectOfType<GameManager>().EndGame();
-        }
-        else if (currentTime <= 3) {
-            text.color = Color.red;
-        }
+            currentTime -= 1 * Time.deltaTime;
+            text.text = currentTime.ToString("0");
 
-       
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                FindObjectOfType<GameManager>().EndGame();
+            }
+            else if (currentTime <= 3)
+            {
+                text.color = Color.red;
+
+            }
+        }
     }
 }
