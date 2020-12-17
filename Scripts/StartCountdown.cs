bf7ced92b41dd0f96fc0dc0966df155d7b16ec78
sprzrg <58468359+sprzrg@users.noreply.github.com>
2020-12-17 15:34:33 +0100

    last changes

---
+++ Scripts/StartCountdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class StartCountdown : MonoBehaviour
+{
+    public float timeLeft = 3.0f;
+    public TextMeshProUGUI startText;
+    public GameObject StartCountdownUI;
+   
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            StartCountdownUI.SetActive(false);
+        }
+    }
+}
